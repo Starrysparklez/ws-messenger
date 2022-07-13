eb49405dfbc6233106c48ee23b35820a309071e0
Starrysparklez <starrysparklez@naomi.win>
2022-07-14 04:25:55 +0700

    oh my cors

--- app/blueprints/api/__init__.py
+++ app/blueprints/api/__init__.py
@@ -1,1 +1,17 @@
-from .routes import api
+from flask import Blueprint
+
+
+api = Blueprint("api", __name__)
+
+@api.after_request
+def add_cors_header(response):
+    print(response)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    return response
+
+from .message import *
+from .channel import *
+from .user import *
+from .socket import *
