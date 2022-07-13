70b44a0b03a3bdd56f506582227f50ca5584a975
Starrysparklez <starrysparklez@naomi.win>
2022-07-14 04:19:52 +0700

    Message id is now str, channel_id and author_id removed, channel and author added

--- app/models/message.py
+++ app/models/message.py
@@ -1,20 +1,26 @@
 from datetime import datetime
+from .channel import Channel
+from .user import User
 
 
 class Message:
     def __init__(self, **kwargs) -> None:
-        self.id: int = kwargs.pop("id", None)
-        self.channel_id: int = kwargs.pop("channel_id", None)
-        self.author_id: int = kwargs.pop("author_id", None)
+        self.id: str = kwargs.pop("id", None)
+        self.channel: Channel = kwargs.pop("channel", None)
+        self.author: User = kwargs.pop("author", None)
         self.content: str = kwargs.pop("content", None)
         self.created_at: datetime = kwargs.pop("created_at", None)
 
     def to_json(self) -> dict:
         """Convert to json."""
+        # send empty user template if message author is unknown
+        if not self.author:
+            print("no author for message", self.id)
+            self.author = User(role='deleted')
         return {
-            "id": str(self.id),
-            "channel_id": str(self.channel_id),
+            "id": self.id,
+            "channel": self.channel.to_json(),
             "content": str(self.content),
-            "author_id": str(self.author_id),
-            "created_at": str(self.created_at.timestamp())
+            "author": self.author.to_json(),
+            "created_at": self.created_at.strftime("%c")
         }
