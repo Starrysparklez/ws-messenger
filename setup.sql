b068f0a5bff8ec9306eb2e08aef263a32d41abcb
Starrysparklez <starrysparklez@naomi.win>
2022-07-14 04:20:35 +0700

    sql scheme rewrite

--- setup.sql
+++ setup.sql
@@ -1,21 +1,24 @@
+--- KEY --------------- TYPE -------------- DEFAULTS ---
 CREATE TABLE IF NOT EXISTS users(
-    id BIGINT PRIMARY KEY NOT NULL,
-    username VARCHAR(24) NOT NULL,
-    role SMALLINT DEFAULT 0,
-    password_hash VARCHAR(120) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    locale_code VARCHAR(5) DEFAULT 'en_US'
+    id                  VARCHAR             PRIMARY KEY NOT NULL,
+    username            VARCHAR(24)         NOT NULL,
+    role                VARCHAR             DEFAULT NULL,
+    password_hash       VARCHAR(120)        NOT NULL,
+    created_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP,
+    locale_code         VARCHAR(5)          DEFAULT 'en_US',
+    discriminator       VARCHAR(4)          DEFAULT '0000'
 );
-CREATE TABLE IF NOT EXISTS text_channels(
-    id BIGINT PRIMARY KEY NOT NULL,
-    name VARCHAR(24) NOT NULL,
-    description VARCHAR(128) DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS channels(
+    id                  VARCHAR             PRIMARY KEY NOT NULL,
+    name                VARCHAR(24)         NOT NULL,
+    topic               VARCHAR(128)        DEFAULT NULL,
+    created_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP,
+    type                VARCHAR(8)          NOT NULL
 );
 CREATE TABLE IF NOT EXISTS messages(
-    id BIGINT PRIMARY KEY NOT NULL,
-    channel_id BIGINT NOT NULL,
-    author_id BIGINT NOT NULL,
-    content VARCHAR(2000) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id                  VARCHAR             PRIMARY KEY NOT NULL,
+    channel_id          VARCHAR             NOT NULL,
+    author_id           VARCHAR             NOT NULL,
+    content             VARCHAR(2000)       NOT NULL,
+    created_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP
 );
